ad826586bbb69de1ebe6b0bb7eec8356066bad0d
m1chael90nisio <32392538+m1chael90nisio@users.noreply.github.com>
2019-05-13 13:25:42 +0300

    Update reqs.js

--- reqs.js
+++ reqs.js
@@ -1,22 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-function calEmAl(){	
-						 
-		Promise.all([
-							axios.get('https://api.thingspeak.com/channels/762750/fields/1/last.json'),
-							axios.get('https://api.thingspeak.com/channels/762750/fields/2/last.json'),
-							axios.get('https://api.thingspeak.com/channels/762750/fields/3/last.json')
-										])
-								// use arrow function to avoid loosing context
-								// BTW you don't need to use axios.spread with ES2015 destructuring
-								.then(([temp, hum,hygr]) => {
-									console.log(temp.data.field1);
-									this.setState({temp:temp.data.field1,hum:hum.data.field2,hygr:hygr.data.field3});
-									
-									});
-				
-				
-				}
+function calEmAl() {
+  Promise.all([
+    axios.get("https://api.thingspeak.com/channels/762750/fields/1/last.json"),
+    axios.get("https://api.thingspeak.com/channels/762750/fields/2/last.json"),
+    axios.get("https://api.thingspeak.com/channels/762750/fields/3/last.json")
+  ])
+    // use arrow function to avoid loosing context
+    // BTW you don't need to use axios.spread with ES2015 destructuring
+    .then(([temp, hum, hygr]) => {
+      console.log(temp.data.field1);
+      this.setState({
+        temp: temp.data.field1,
+        hum: hum.data.field2,
+        hygr: hygr.data.field3
+      });
+    });
+}
 
-
-export{calEmAl};
+export { calEmAl };
