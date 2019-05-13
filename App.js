14157d0aafb76f09365d9ad48801ebe48316ab96
m1chael90nisio <32392538+m1chael90nisio@users.noreply.github.com>
2019-05-13 13:25:08 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,54 +1,49 @@
-import React from 'react' ;
-import './App.css';
-import './bootstrap.min.css';
-import {calEmAl} from './reqs.js';
+import React from "react";
+import "./App.css";
+import "./bootstrap.min.css";
+import { calEmAl } from "./reqs.js";
 
 class App extends React.Component {
-	constructor() {
+  constructor() {
     super();
-	this.state = {
-    temp: "",
-	hum:"",
-	hygr:"",
-	time:new Date().toLocaleString(),
-	checked: false,
-	led : "OFF",
+    this.state = {
+      temp: "",
+      hum: "",
+      hygr: "",
+      time: new Date().toLocaleString(),
+      checked: false,
+      led: "OFF"
+    };
   }
-  
-	}
-    
-	componentDidMount() {
-		this.intervalID = setInterval(
-		  () => this.reseter(),
-		  1000
-		);
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.reseter(), 1000);
   }
 
   componentWillUnmount() {
-		clearInterval(this.intervalID);
+    clearInterval(this.intervalID);
   }
-  
+
   reseter() {
-		this.setState({
-		  time: new Date().toLocaleString()
-		});
-const cl=calEmAl.bind(this);
-cl();
+    this.setState({
+      time: new Date().toLocaleString()
+    });
+    const cl = calEmAl.bind(this);
+    cl();
+  }
 
-			  } 
-  
-  render(){
-    return(
-      <div >
-	  <p>
-	   Humidity: {this.state.hum}% </p>
-	  <p>
-  Temperature:{this.state.temp}&#8451; </p>
-	  <p>
-	   Soil Hygrometer:{this.state.hygr} </p>
+  render() {
+    return (
+      <div className="parent">
+        <h1 className="hf">Gardening sensors data</h1>
 
-	  Time:{this.state.time}
-	  </div>	
+        <div className="box">
+          <p>Humidity: {this.state.hum}% </p>
+          <p>Temperature: {this.state.temp}&#8451; </p>
+          <p>Soil Hygrometer: {this.state.hygr} </p>
+          Time: {this.state.time}
+        </div>
+      </div>
     );
   }
 }
